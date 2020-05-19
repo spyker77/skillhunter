022a4f928141d0d9adf224b1e3d30844686f70a3
Your Name <evgeni.sautin@gmail.com>
2020-05-19 21:44:19 +0300

    Changed ALLOWED_HOSTS

--- skillhunter_project/settings.py
+++ skillhunter_project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["skillhunter-app.herokuapp.com/", "localhost", "127.0.0.1"]
 
 
 # Application definition
