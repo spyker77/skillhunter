3a82e2fd54a1a5b88b0ef208b6021edd7af9f687
Your Name <evgeni.sautin@gmail.com>
2020-05-19 21:51:27 +0300

    WhiteNoise added to INSTALLED_APPS

--- skillhunter_project/settings.py
+++ skillhunter_project/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["skillhunter-app.herokuapp.com/", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["skillhunter-app.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     # Local apps
     "scraping",
