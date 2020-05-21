35fc3846c360cd1af4afb57743a4b788b8fea03a
Your Name <evgeni.sautin@gmail.com>
2020-05-21 13:42:34 +0300

    Required added to the input field

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,15 @@
+// $(document).ready(function () {
+//     $('#form-input').keypress(function (event) {
+//         var keycode = (event.keyCode ? event.keyCode : event.which);
+//         if (keycode == '13') {
+//             $("#reminder").show();
+//         };
+//     });
+// });
+
+
 $(document).ready(function () {
-    $('#form-input').keypress(function (event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode == '13') {
-            $("#reminder").toggle();
-        };
+    $("#home-form").submit(function () {
+        $("#reminder").show();
     });
 });
