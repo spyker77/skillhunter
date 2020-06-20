3a64c771821c51eb16242d6d4b05a8a173ea7342
Your Name <evgeni.sautin@gmail.com>
2020-06-20 18:11:47 +0300

    DjangoTemplates engine replaced by Jinja2

--- skillhunter_project/settings.py
+++ skillhunter_project/settings.py
@@ -65,25 +65,21 @@ ROOT_URLCONF = "skillhunter_project.urls"
 
 TEMPLATES = [
     {
+        "BACKEND": "django.template.backends.jinja2.Jinja2",
+        "DIRS": [os.path.join(BASE_DIR, "jinja2")],
+        "APP_DIRS": True,
+        "OPTIONS": {"environment": "skillhunter_project.jinja2.environment",},
+    },
+    {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [os.path.join(BASE_DIR, "templates")],
-        # Commented, as this is a requirement for loaders (see below).
-        # "APP_DIRS": True,
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ],
-            "loaders": [
-                (
-                    "django.template.loaders.cached.Loader",
-                    [
-                        "django.template.loaders.filesystem.Loader",
-                        "django.template.loaders.app_directories.Loader",
-                    ],
-                ),
             ],
         },
     },
