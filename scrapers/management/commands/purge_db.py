1b2b825ce248b036660fce15d11f599b5b7f4f24
Your Name <evgeni.sautin@gmail.com>
2020-07-05 10:45:41 +0300

    Added counter for outdated vacancies to delete

--- scrapers/management/commands/purge_db.py
+++ scrapers/management/commands/purge_db.py
@@ -10,5 +10,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Delete records older than 7 days (before scraping new data).
         past = timezone.now() - timezone.timedelta(days=7)
-        Vacancy.objects.filter(created_date__lte=past).delete()
-        self.stdout.write("🗂️ Database successfully cleaned from outdated vacancies.")
+        outdated_vacancies = Vacancy.objects.filter(created_date__lte=past)
+        amount = outdated_vacancies.count()
+        outdated_vacancies.delete()
+        self.stdout.write(
+            f"🗂️ Database successfully cleaned from {amount} outdated vacancies."
+        )
