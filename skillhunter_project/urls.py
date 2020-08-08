15a2ff0a4c30d8b502d783d75f7ac498fa3e609a
Your Name <evgeni.sautin@gmail.com>
2020-08-08 18:24:42 +0300

    Updated packages including Django 3.1

--- skillhunter_project/urls.py
+++ skillhunter_project/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
-from django.conf.urls import url
 from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from django.contrib.sitemaps.views import sitemap
 
 from pages.sitemaps import StaticViewSitemap
@@ -24,7 +23,7 @@ urlpatterns = [
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
-    url(r"^robots\.txt", include("robots.urls")),
+    re_path(r"^robots\.txt", include("robots.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
