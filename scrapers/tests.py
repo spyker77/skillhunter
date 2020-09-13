b2b9c1c3d866cfedf1491918642ba9fd7e35b736
Your Name <evgeni.sautin@gmail.com>
2020-09-13 19:25:33 +0300

    Added test_searchresultslistview_creates_new_search_object

--- scrapers/tests.py
+++ scrapers/tests.py
@@ -1,17 +1,32 @@
 from django.urls import reverse, resolve
 
 from .views import SearchResultsListView
+from .models import Search
 
 import pytest
 
 
 @pytest.mark.django_db
 class TestSearchResultsListView:
+    url = reverse("search_results")
+    data = {"q": "test search query"}
+    query = data.get("q")
+    ip_address = "0.0.0.0"
+    user_agent = "Test User-Agent"
+
     @pytest.fixture
-    def response(self, client):
-        self.url = reverse("search_results")
-        test_query = "?q=junior+python+developer"
-        return client.get(self.url + test_query)
+    def mock_environment_production(self, monkeypatch):
+        monkeypatch.setenv("ENVIRONMENT", "production")
+
+    @pytest.fixture
+    def response(self, monkeypatch, rf):
+        request = rf.get(
+            self.url,
+            self.data,
+            REMOTE_ADDR=self.ip_address,
+            HTTP_USER_AGENT=self.user_agent,
+        )
+        return SearchResultsListView.as_view()(request)
 
     def test_searchresultslistview_status_code(self, response):
         assert response.status_code == 200
@@ -22,9 +37,17 @@ class TestSearchResultsListView:
     def test_searchresultslistview_contains_correct_html(self, response):
         assert "Skills you need to be a" in response.rendered_content
 
-    def test_searchresultslistview_does_not_contains_incorrect_html(self, response):
+    def test_searchresultslistview_does_not_contain_incorrect_html(self, response):
         assert "Hi there! I should not be on the page." not in response.rendered_content
 
     def test_searchresultslistview_url_resolves_searchresultslistview(self):
-        self.view = resolve("/search/")
-        assert self.view.func.__name__ == SearchResultsListView.as_view().__name__
+        view = resolve("/search/")
+        assert view.func.__name__ == SearchResultsListView.as_view().__name__
+
+    def test_searchresultslistview_creates_new_search_object(
+        self, mock_environment_production, response
+    ):
+        new_search_object = Search.objects.filter(user_agent=self.user_agent)
+        assert new_search_object[0].query == self.query
+        assert new_search_object[0].ip_address == self.ip_address
+        assert new_search_object[0].user_agent == self.user_agent
